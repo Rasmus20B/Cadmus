b0eb3561c4ddb89627271893d7b50580b92a126b
JT <jmtateossian50b@gmail.com>
2025-03-08 21:04:33 +0000

    updated flake to include openssl library location

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
       in
       {
         devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.pkg-config ];
           buildInputs = [
           pkgs.rustc
           pkgs.cargo
@@ -23,9 +24,12 @@
           pkgs.postgresql
           pkgs.docker
           pkgs.docker-compose
+          pkgs.openssl
 
           pkgs.nodejs
         ];
+
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
 
         shellHook = ''
           export DATABASE_URL=postgres://user:password@localhost/cadmus_db
